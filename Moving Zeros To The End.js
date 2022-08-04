e59773bc0c21908245a540840669ee3efbee08c3
t-creates <geislinger@pm.me>
2022-08-04 10:42:20 -0600

    Moving Zeros To The End

--- Moving Zeros To The End.js
+++ Moving Zeros To The End.js
@@ -1,10 +1,25 @@
-function moveZeros(arr) {
-  return arr.sort(function(a) {
-       if (a === 0) {
-         arr.pop(a)
-         return arr.push(a)
-       }
-    });   
-};
+/* Moving Zeros To The End
 
-console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) ---> returns[false,1,1,2,1,3,"a",0,0]
+
+
+*/
+// Solution #0
+const moveZeros = arr => {
+  let withoutZeros = arr.filter(elm => elm === 0);
+  let withZeros = arr.filter(elm => elm !== 0);
+  return withZeros.concat(withoutZeros);
+}
+
+// Solution #1
+var moveZeros = function (arr) {
+  for(var i = arr.length - 1; i >= 0; i--) {
+    if(arr[i] === 0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    }
+  }
+  return arr;
+}
